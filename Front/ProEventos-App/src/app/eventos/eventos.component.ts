48356ec200d9a75283d296f9f674460de118aee7
AzureAD\FELIPEDASILVAPESTANA <felipe-pe27@hotmail.com>
2022-04-05 20:49:42 -0300

    add: bootstrap e fontawsone

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,23 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public envetos: any = [
-    {
-    Tema: 'Angular',
-    Local: "SP"
-    },
-    {
-      Tema: 'NET 5',
-      Local: "Rio Preto"
-    },
-    {
-      Tema: 'Angular Suas Novidades',
-      Local: "Campinas"
-      }
-  ]
-  constructor() { }
+  public envetos: any;
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.getEventos();
+  }
+
+  public getEventos(): void {
+    this.http.get("https://localhost:5001/api/eventos").subscribe(
+      response => this.envetos = response,
+      error => console.log(error),
+    );
+    this.envetos = [
+      {
+      Tema: 'Angular',
+      Local: "SP"
+      },
+      {
+        Tema: 'NET 5',
+        Local: "Rio Preto"
+      },
+      {
+        Tema: 'Angular Suas Novidades',
+        Local: "Campinas"
+        }
+    ]
   }
 
 }
