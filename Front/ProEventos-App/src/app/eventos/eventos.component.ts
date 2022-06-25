96537adb1e462eea290e22011a2bba648143832b
AzureAD\FELIPEDASILVAPESTANA <felipe-pe27@hotmail.com>
2022-06-25 16:53:53 -0300

    fix: ngfor, ng-if fazendo a requisição no banco de dados

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos: any;
+  public eventos: any = [];
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
@@ -17,23 +17,27 @@ export class EventosComponent implements OnInit {
 
   public getEventos(): void {
     this.http.get("https://localhost:5001/api/eventos").subscribe(
+
       response => this.eventos = response,
+
       error => console.log(error),
     );
-    this.eventos = [
-      {
-      Tema: 'Angular',
-      Local: "SP"
-      },
-      {
-        Tema: 'NET 5',
-        Local: "Rio Preto"
-      },
-      {
-        Tema: 'Angular Suas Novidades',
-        Local: "Campinas"
-        }
-    ]
+
+
+    // this.eventos = [
+    //   {
+    //   Tema: 'Angular',
+    //   Local: "SP"
+    //   },
+    //   {
+    //     Tema: 'NET 5',
+    //     Local: "Rio Preto"
+    //   },
+    //   {
+    //     Tema: 'Angular Suas Novidades',
+    //     Local: "Campinas"
+    //     }
+    // ]
   }
 
 }
